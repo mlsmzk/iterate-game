4b689fc242e547eb6bb4f1b0e585cf6d53bcb19f
Miles Mezaki <mmezaki@olin.edu>
2022-09-18 20:39:10 -0400

    Add experiences and properties

--- iterate_game.py
+++ iterate_game.py
@@ -1,6 +1,51 @@
 import sys
 import random
 
+# Intellect
+# Physique
+# Wealth
+# Connections
+# Leadership 
+# Fame
+
+class Experience():
+    def __init__(self, name, gain, loss):
+        self.data = {"name" : name,
+                     "gain" : gain,
+                     "loss" : loss}
+
+    def __repr__(self):
+        s = ""
+        for k,v in self.data.items():
+            s += (k + " : " + v + "\n")
+        return s
+
+class Property():
+    def __init__(self, name, desc, gain, loss):
+        self.data = {"name" : name,
+                     "desc" : desc,
+                     "gain" : gain,
+                     "loss" : loss}
+    def __repr__(self):
+        s = ""
+        for k,v in self.data.items():
+            s += (k + " : " + v + "\n")
+        return s
+
+def property_select():
+    properties = {
+        2: Property("Athleticism", "You do a sport during HS or College", "+5 physique, +3 connections", "None"),
+        3: Property("Artistic", "You do a art during HS or College", "+3 fame, +3 connections", "None"),
+        4: Property("Leadership", "Anytime you draw a leadership experience card, double the leadership gain", "x2 leadership", "None"),
+        5: Property("Single Income Family", "If there is a competition event, or in a fight for college, receive a special boost", "+10 stats required to win", "-10 wealth"),
+        6: Property("First Generation", "If there is a competition event, or in a fight for college, roll the die to receive a bonus equal to the number on the die", "Number on the die", "-5 wealth, -5 connections"),
+        7: Property("Rich Family", "Gain connections and wealth upon drawing this card", "+10 connections, +10 wealth", "None"),
+        8: Property("Impoverished", "All gains in wealth are halved", "Roll die in school selection phase. If the number on the die > 4, add the die's number to the desired stat.", "All wealth gains are worth half as much"),
+        9: Property("Family Alumni", "In college selection, gain stat value.", "+10 all required stats for college admission", "-5 fame"),
+        10: Property("Mafia", "We'd tell you but we'd have to kill you.", "+10 wealth, +15 connections", "-10 fame")
+        }
+    return properties[random.randrange(2, 11)]
+
 class School():
     def __init__(self, name, reqs, unlocks, pref):
         self.data = {"name" : name,
@@ -49,10 +94,15 @@ def school_select(arg):
         50 : School("Lavender", "intellect > 15 and connections > 10", ("c", "lvl3"), "connections"),
     
         62 : School("Olin College of Engineering", "intellect > 50, or intellect > 35, wealth > 10 and leadership > 10", "lvl5", "intellect"),
-        63 : School("Harvard", "intellect > 25, connections > 20, wealth > 25", "lvl5", "wealth")
+        63 : School("Harvard", "intellect > 25, connections > 20, wealth > 25", "lvl5", "wealth"),
+        64 : School("Community College", "total > 30", "lvl5", "None"),
+        65 : School("UCLA", "intellect > 35, physique > 10, wealth > 5", "lvl5", "None"),
+        66 : School("Stanford", "intellect > 50, wealth > 10, connections > 10, or fame > 12", "lvl5", "fame"),    
+        67 : School("UC Riverside", "intellect > 20, total > 40", "lvl5", "None"),
+        68 : School("University of Illinois Urbana-Champaign", "intellect > 20, total > 40", "lvl5", "None")
     }
     if arg == "c":
-        return schools[random.randrange(62,64)]
+        return schools[random.randrange(62,69)]
     if arg == "hs":
         return schools[random.randrange(42,51)]
     if arg == "ms":
@@ -64,4 +114,6 @@ def school_select(arg):
 if __name__ == "__main__":
     if sys.argv[1] == "school":
         card = school_select(sys.argv[2])
+    elif sys.argv[1] == "property":
+        card = property_select()
     print(card)
